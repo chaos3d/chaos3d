536d703c512d4cc51860b80fdbf721bf1b84f40c
Hao Wu <wuhao.wise@gmail.com>
2013-01-20 23:34:43 -0800

    common.h

---
+++ src/common.h
@@ -0,0 +1,28 @@
+#ifndef _CHAOS_COMMON_H
+#define _CHAOS_COMMON_H
+
+#define _CHAOS_BEGIN
+#define _CHAOS_END
+
+#if defined(WIN32)
+
+#	define CHAOS_PRIVATE
+#	if !defined(STATIC_LIB)
+#	define	CHAOS_API
+#	else
+#		if defined(DLL_EXPORT)
+#		define	CHAOS_API	__declspec(dllexport)
+#		else
+#		define	CHAOS_API	__declspec(dllimport)
+#		endif	// DLL_EXPORT
+#	endif	// STATIC_LIB
+
+// WIN32
+#elif defined(__GNUC__)
+
+#	define	CHAOS_API		__attribute__((visibility("default")))
+#	define	CHAOS_PRIVATE	__attribute__((visibility("hidden")))
+
+#endif	// __GNU_C__
+
+#endif
