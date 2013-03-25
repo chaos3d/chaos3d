28fe332b067564e1c4b752d4748ce1495dabe2d6
Hao Wu <wuhao.wise@gmail.com>
2013-03-25 01:37:58 -0700

    adding sample

--- sample/SampleBrowser/SampleBrowser/ViewController.m
+++ sample/SampleBrowser/SampleBrowser/ViewController.m
@@ -14,16 +14,61 @@
 
 @implementation ViewController
 
+@synthesize sampleControllers;
+
+- (void) dealloc{
+    self.sampleControllers = nil;
+    [super dealloc];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.sampleControllers = [NSDictionary dictionaryWithObjectsAndKeys: [UINavigationController class], @"Name", nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table View Source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [sampleControllers count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+	static NSString *kCustomCellID = @"CustomCellID";
+	
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCustomCellID];
+	if (cell == nil)
+	{
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:kCustomCellID] autorelease];
+	}
+    
+	NSArray* keys = sampleControllers.allKeys;
+	cell.textLabel.text = [keys objectAtIndex: indexPath.row];
+	//cell.detailTextLabel.text =
+	
+	return cell;
+}
+
+#pragma mark - Table View Delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+	NSArray* keys = sampleControllers.allKeys;
+	NSString* key = [keys objectAtIndex: indexPath.row];
+    Class klz = [sampleControllers objectForKey:key];
+    UIViewController* controller = [[[klz alloc] init] autorelease];
+    [self presentViewController:controller animated:YES
+                     completion:^{
+                         nil;
+                     }];
 }
 
 @end
