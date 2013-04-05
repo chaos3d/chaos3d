c09cb6aa025906131c20661a1dc099406eff3cbe
Hao Wu <wuhao.wise@gmail.com>
2013-04-04 23:08:19 -0700

    several projs

---
+++ src/platform/ios/cViewController.h
@@ -0,0 +1,13 @@
+//
+//  cViewController.h
+//  platform-ios
+//
+//  Created by Hao Wu on 3/27/13.
+//  Copyright (c) 2013 cHaos3D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cViewController : UIViewController
+
+@end
---
+++ src/platform/ios/cViewController.m
@@ -0,0 +1,38 @@
+//
+//  cViewController.m
+//  platform-ios
+//
+//  Created by Hao Wu on 3/27/13.
+//  Copyright (c) 2013 cHaos3D. All rights reserved.
+//
+
+#import "cViewController.h"
+
+@interface cViewController ()
+
+@end
+
+@implementation cViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
