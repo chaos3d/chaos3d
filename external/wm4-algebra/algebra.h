e30460450795e15739d0e08b1171296059c453ee
Hao Wu <wuhao.wise@gmail.com>
2013-04-06 02:13:46 -0700

    fixing scene2dnode

---
+++ external/wm4-algebra/algebra.h
@@ -0,0 +1,14 @@
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Vector4.h"
+
+#include "Matrix2.h"
+#include "Matrix3.h"
+#include "Matrix4.h"
+
+#include "Quaternion.h"
+#include "ColorRGB.h"
+#include "ColorRGBA.h"
+
+#include "Rectangle.h"
+#include "Rectangle3.h"
