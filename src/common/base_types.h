4fd0fdcb326b0d469508abf98c72df87516d6edf
Hao Wu <wuhao.wise@gmail.com>
2014-02-09 15:47:21 -0800

    base type

---
+++ src/common/base_types.h
@@ -0,0 +1,11 @@
+#ifndef _BASE_TYPES_H
+#define _BASE_TYPES_H
+
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
+
+typedef Eigen::Vector2f vector2f;
+typedef Eigen::Vector3f vector3f;
+typedef Eigen::Vector4f vector4f;
+
+#endif
