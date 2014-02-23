b8f65923273a474bbfb9d461199cca7c2c436d5a
Hao Wu <wuhao.wise@gmail.com>
2014-02-23 04:22:59 -0800

    json wrapper

--- src/io/json_loader.cpp
+++ src/io/json_loader.cpp
@@ -1,1 +1,20 @@
 #include "io/json_loader.h"
+#include "memory_stream.h"
+#include <cJSON/cJSON.h>
+
+json_loader::json_loader(char const* str) {
+    const char* parse_end = nullptr;
+    _json_root = cJSON_ParseWithOpts(str, &parse_end, 1);
+    
+    // TODO: throw instead?
+    assert(parse_end == str + strlen(str));
+}
+
+json_loader::json_loader(memory_stream* stream)
+: json_loader(stream->address()) { 
+}
+
+json_loader::~json_loader() {
+    if (_json_root != NULL)
+        cJSON_Delete(_json_root);
+}
--- src/io/json_loader.h
+++ src/io/json_loader.h
@@ -2,19 +2,39 @@
 #define _CHAOS3D_JSON_LOADER_H
 
 #include <memory>
-#include "io/data_stream.h"
 
-// TODO: loader interface
+struct cJSON;
+class memory_stream;
+
+// The JSON wrapper for cJSON, this serves to remove
+// dependencies between the client code and cJSON
 class json_loader {
 public:
-    // take the stream as the fundamental data source
-    template<class... Args>
-    json_loader(Args&&... args)
-    : _stream(std::forward<Args>(args)...)
-    {}
+    typedef int position_t;
     
+public:
+    json_loader(char const*);
+    json_loader(memory_stream*);
+    
+    json_loader(json_loader&& rhs)
+    : _json_root(rhs._json_root) {
+        rhs._json_root = NULL;
+    }
+    
+    json_loader& operator=(json_loader&& rhs) {
+        _json_root = rhs._json_root;
+        rhs._json_root = NULL;
+        return *this;
+    }
+    
+    ~json_loader();
+    
+    json_loader(json_loader const&) = delete;
+    json_loader& operator=(json_loader const&) = delete;
+    
+    cJSON* internal() const { return _json_root; }
 private:
-    data_stream::ptr _stream;
+    cJSON* _json_root;
 };
 
 #endif
