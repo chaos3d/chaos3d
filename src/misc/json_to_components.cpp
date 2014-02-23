c6d3179f087d9e2e49ba76688fdde69164ad6567
Hao Wu <wuhao.wise@gmail.com>
2014-02-23 15:01:46 -0800

    remove io converter

--- src/misc/json_to_components.cpp
+++
@@ -1,6 +0,0 @@
-#include "misc/json_to_components.h"
-
-bool io_converter<json_loader, sprite2d::camera2d>::
-operator()(json_loader const&, sprite2d::camera2d &) const {
-    return false;
-}
--- src/misc/json_to_texture_atlas.h
+++ src/misc/json_to_texture_atlas.h
@@ -1,14 +1,8 @@
 #ifndef _CHAOS3D_JSON_TO_TEXTURE_ATLAS_H
 #define _CHAOS3D_JSON_TO_TEXTURE_ATLAS_H
 
-#include "io/io_converter.h"
 #include "io/json_loader.h"
 #include "com/sprite2d/texture_atlas.h"
-
-template<>
-struct io_converter<json_loader, texture_atlas> {
-    bool operator() (json_loader const&, texture_atlas&) const;
-};
 
 template<>
 std::pair<bool, json_loader::position_t> texture_atlas::load_from<json_loader>(json_loader const&);
