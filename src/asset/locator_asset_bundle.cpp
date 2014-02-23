591f28208ffcf2701a27499d3e80e3d1ee1b313f
Hao Wu <wuhao.wise@gmail.com>
2014-02-23 02:39:35 -0800

    helper asset bundle using locators

---
+++ src/asset/locator_asset_bundle.cpp
@@ -0,0 +1,20 @@
+#include "asset/locator_asset_bundle.h"
+
+locator_asset_bundle::locator_asset_bundle(asset_locator::ptr const& locator,
+                                           std::string const& meta_extension)
+: _locator(locator), _extension(meta_extension) {
+    
+}
+
+locator_asset_bundle::names_t locator_asset_bundle::all_names() const {
+    names_t names;
+    _locator->traverse([&] (std::string const& name) {
+        auto pre = name.length() - _extension.length();
+        if (name.length() < _extension.length())
+            return;
+        else if (std::memcmp(_extension.c_str(),
+                             name.c_str() + pre, _extension.length()) == 0)
+            names.emplace_back(name.c_str(), name.length() - _extension.length());
+    });
+    return names;
+}
---
+++ src/asset/locator_asset_bundle.h
@@ -0,0 +1,29 @@
+#ifndef _CHAOS3D_ASSET_LOCATOR_ASSET_BUNDLE_H
+#define _CHAOS3D_ASSET_LOCATOR_ASSET_BUNDLE_H
+
+#include "asset/asset_bundle.h"
+#include "asset/asset_locator.h"
+
+// The asset bundle from the given path(locator) and the
+// extension for a particular file type
+// This is a helper bundle for the assets from the files of
+// the same type (.png); this may only produce one type
+// of resources (texture). The file type itself can be
+// a meta or produce several types of resources as well.
+class locator_asset_bundle : public asset_bundle {
+public:
+    locator_asset_bundle(asset_locator::ptr const& locator,
+                         std::string const& meta_extension = "");
+    
+    virtual names_t all_names() const override;
+    
+protected:
+    asset_locator::ptr const& locator() const { return _locator;}
+    std::string const& extension() const { return _extension; }
+    
+private:
+    std::string _extension;
+    asset_locator::ptr _locator;
+};
+
+#endif
