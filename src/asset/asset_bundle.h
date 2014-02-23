90feebfb394416fdd8aea1e3512ea591c9e0c2f6
Hao Wu <wuhao.wise@gmail.com>
2014-02-23 02:38:13 -0800

    clean up asset bundle

--- src/asset/asset_bundle.h
+++ src/asset/asset_bundle.h
@@ -7,55 +7,36 @@
 #include <vector>
 #include "common/utility.h"
 
-class asset_handle : public std::enable_shared_from_this<asset_handle> {
-public:
-    typedef std::shared_ptr<asset_handle> ptr;
+class asset_handle;
 
-public:
-    template<class T>
-    typename std::remove_cv<T>::type& as() {
-        return static_cast<typename std::remove_cv<T>::type&>(*this);
-    }
-    
-    template<class Loader, class... Args>
-    bool load_from(Loader&&, Args&&...);
-    
-    // TODO: thread-safe for async loading
-    virtual bool is_loaded() const = 0;
-    virtual void load() = 0;
-    virtual void unload() = 0;
-    
-    // weak reference to the actual asset
-    // virtual uint32_t use_count() const = 0;
-    virtual ~asset_handle() {};
-};
-
-// asset_handle placeholder
-// any asset needs to specialize this class
-// this is to prevent from that any asset would be inheriting
-// from the common parent, to remove dependencies
-template<class T>
-class asset_handle_base;
-
-// asset meta directory
+// The assets container.
+// It 'contains' the asset mete info (asset handle) in physical
+// form (zip bundle) or virtually (a list of files). It won't
+// load or manage the actual asset but create the handle so the
+// asset can be lazily loaded by the asset manager
 class asset_bundle : public std::enable_shared_from_this<asset_bundle> {
 public:
-    typedef std::shared_ptr<asset_handle> asset_ptr;
-    typedef std::vector<std::pair<std::string, asset_ptr>> assets_t;
+    typedef std::shared_ptr<asset_bundle> ptr;
+    typedef std::shared_ptr<asset_bundle> const_ptr;
+
+    typedef std::shared_ptr<asset_handle> handle_ptr;
+    typedef std::vector<std::pair<std::string, handle_ptr>> handles_t;
+    typedef std::vector<std::string> names_t;
     
 public:
     virtual ~asset_bundle() {};
     
-    // load a new meta from the given name
+    // get a new meta from the given name, the asset shouldn't
+    // be loaded (asset_handle == nullptr)
     // this will also produce a new asset since
     // the meta will be different
-    virtual asset_ptr load(std::string const&) const = 0;
-    
-    // find all the assets meta in this bundle
-    virtual assets_t all_assets() const = 0;
+    virtual handle_ptr get(std::string const&) const = 0;
     
     // find all the assets names
-    virtual std::vector<std::string> all_names() const = 0;
+    virtual names_t all_names() const = 0;
+
+    // get all the assets meta in this bundle
+    virtual handles_t all_assets() const;
 };
 
 #endif
