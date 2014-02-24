fa643ca3deda85a2034df2b57694afc8d2628f4c
Hao Wu <wuhao.wise@gmail.com>
2014-02-23 22:49:56 -0800

    relocator loaders

--- src/misc/json_to_texture_atlas.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CHAOS3D_JSON_TO_TEXTURE_ATLAS_H
-#define _CHAOS3D_JSON_TO_TEXTURE_ATLAS_H
-
-#include "io/json_loader.h"
-#include "com/sprite2d/texture_atlas.h"
-
-template<>
-std::pair<bool, json_loader::position_t> texture_atlas::load_from<json_loader>(json_loader const&);
-#endif
