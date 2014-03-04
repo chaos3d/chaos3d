a4da23b6f0eef94298a9fa713d46b4e43a9d04f5
Hao Wu <wuhao.wise@gmail.com>
2014-03-04 00:36:26 -0800

    dumb notification/comments

--- src/app/application.h
+++ src/app/application.h
@@ -4,15 +4,27 @@
 #include "common/singleton.h"
 #include "event/event_listener.h"
 
+namespace evt {
+    class memory_warning : public event {
+    };
+    
+    class notification : public event {
+    };
+}
+
 class application : public event_listener, public singleton<application> {
 public:
     //
     // common template methods rather than events
     
-    // called after the system is warmed up
+    // called before everything is initialized
+    //  this should be to initialize global singletons etc
+    //  but don't touch any part of engine
     virtual void on_initialize() = 0;
     
     // called before the run loop starts
+    //  this should be to initialize components, actions to
+    //  actually start the game
     virtual void on_launch() = 0;
 };
 
