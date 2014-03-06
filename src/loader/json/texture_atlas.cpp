d82e9d7ece28e8107cd1cd6301e3caafcc661225
Hao Wu <wuhao.wise@gmail.com>
2014-03-06 01:02:30 -0800

    first version to load tps

--- src/loader/json/texture_atlas.cpp
+++ src/loader/json/texture_atlas.cpp
@@ -10,11 +10,11 @@ using namespace rapidjson;
 // texture_atlas loader for texture packer in json
 template<>
 texture_atlas::texture_atlas(json_loader const& json,
-                                      asset_manager& mgr,
-                                      texture_atlas::TexturePacker &&) {
+                             asset_manager& mgr,
+                             texture_atlas::TexturePacker &&) {
     Document const& root = json.internal<Document>();
 
-    std::string file_name(root["metadata"]["target"]["textureFileName"].GetString());
+    std::string file_name(root["meta"]["image"].GetString());
     
     if (!mgr.contains(file_name)) {
         assert(0); // FIXME: manual add meta, and load
@@ -24,10 +24,18 @@ texture_atlas::texture_atlas(json_loader const& json,
     auto size = _texture->size();
 
     auto& frames = root["frames"];
-    for (auto it = frames.MemberBegin(); it != frames.MemberEnd(); ++it) {
-        _rects.emplace(std::piecewise_construct, std::forward_as_tuple(it->name.GetString()),
-                       std::forward_as_tuple(Eigen::Vector2f{ (float)0 / size.x(), (float)0 / size.y()},
-                                             Eigen::Vector2f{ (float)64 / size.x(), (float)64 / size.y()}));
-
+    for (auto it =frames.Begin(); it != frames.End(); ++it) {
+        // TODO: source size/rotate/trim
+        auto& frame = (*it)["frame"];
+        float x = (float)frame["x"].GetInt() / size.x();
+        float y = (float)frame["y"].GetInt() / size.y();
+        _rects.emplace(std::piecewise_construct, std::forward_as_tuple((*it)["filename"].GetString()),
+                       std::forward_as_tuple(
+                                             Eigen::Vector2f{x, y},
+                                             Eigen::Vector2f{
+                                                 (float)frame["w"].GetInt() / size.x() + x,
+                                                 (float)frame["h"].GetInt() / size.y() + y}
+                                             )
+                       );
     }
 }
