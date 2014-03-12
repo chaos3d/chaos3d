fd65d49cb435edb6029807213b889523c5c978a0
Hao Wu <wuhao.wise@gmail.com>
2014-03-12 03:45:42 -0700

    lua friendly w/o contructor

--- src/loader/json/texture_atlas.cpp
+++ src/loader/json/texture_atlas.cpp
@@ -9,9 +9,10 @@ using namespace rapidjson;
 
 // texture_atlas loader for texture packer in json
 template<>
-texture_atlas::texture_atlas(json_loader const& json,
-                             asset_manager& mgr,
-                             texture_atlas::TexturePacker &&) {
+texture_atlas texture_atlas::load_from(json_loader const& json,
+                                       asset_manager& mgr,
+                                       texture_atlas::TexturePacker &&) {
+    texture_atlas atlas;
     Document const& root = json.internal<Document>();
 
     std::string file_name(root["meta"]["image"].GetString());
@@ -20,22 +21,32 @@ texture_atlas::texture_atlas(json_loader const& json,
         assert(0); // FIXME: manual add meta, and load
     }
 
-    _texture = mgr.load<texture>(file_name);
-    auto size = _texture->size();
+    atlas._texture = mgr.load<texture>(file_name);
+    auto size = atlas._texture->size();
 
     auto& frames = root["frames"];
+    auto& rects = atlas._rects;
     for (auto it =frames.Begin(); it != frames.End(); ++it) {
         // TODO: source size/rotate/trim
         auto& frame = (*it)["frame"];
         float x = (float)frame["x"].GetInt() / size.x();
         float y = (float)frame["y"].GetInt() / size.y();
-        _rects.emplace(std::piecewise_construct, std::forward_as_tuple((*it)["filename"].GetString()),
-                       std::forward_as_tuple(
-                                             Eigen::Vector2f{x, y},
-                                             Eigen::Vector2f{
-                                                 (float)frame["w"].GetInt() / size.x() + x,
-                                                 (float)frame["h"].GetInt() / size.y() + y}
-                                             )
-                       );
+        rects.emplace(std::piecewise_construct, std::forward_as_tuple((*it)["filename"].GetString()),
+                      std::forward_as_tuple(
+                                            Eigen::Vector2f{x, y},
+                                            Eigen::Vector2f{
+                                                (float)frame["w"].GetInt() / size.x() + x,
+                                                (float)frame["h"].GetInt() / size.y() + y}
+                                            )
+                      );
     }
+    
+    return atlas;
+}
+
+#include "script/lua_ref.h"
+
+template<>
+texture_atlas texture_atlas::load_from(script::ref const& json) {
+    return texture_atlas();
 }
