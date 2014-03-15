5feb34f1c12a4193c428f5bd6f3cf8c1e978e2c2
Hao Wu <wuhao.wise@gmail.com>
2014-03-15 01:30:46 -0700

    fix copy/paste

--- src/script/type/eigen.cpp
+++ src/script/type/eigen.cpp
@@ -3,16 +3,7 @@
 #include "script/lua_bind.h"
 
 namespace script {
-    void def_render_device() {
-        script::class_<render_device>::type()
-        .def("create_shader", LUA_BIND(&render_device::create_shader))
-        ;
-        
-        script::class_<gpu_shader>::type()
-        .def("compile", LUA_BIND_S(void (gpu_shader::*)(char const*), &gpu_shader::compile))
-        .def("compile_multi", LUA_BIND_S(void (gpu_shader::*)(std::vector<char const*> const&),
-                                         &gpu_shader::compile))
-        ;
+    void def_eigen_math() {
         
     }
 }
