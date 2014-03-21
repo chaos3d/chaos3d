f96b303a74534221e9f44ef8e408c515beb76ad8
Hao Wu <wuhao.wise@gmail.com>
2014-03-21 01:47:39 -0700

    current time

--- src/action/action_script.cpp
+++ src/action/action_script.cpp
@@ -8,17 +8,19 @@ bool action_script::done() const {
 
 void action_script::on_start() {
     action::on_start();
-    double delta = global_timer_base::instance().recent_delta();
-    _yielded.reset(std::get<1>(_coroutine.resume<yieldable*>(delta)));
+    auto& timer = global_timer_base::instance();
+    _yielded.reset(std::get<1>(_coroutine.resume<yieldable*>(timer.current_time(),
+                                                             timer.recent_delta())));
 }
 
 void action_script::update() {
     action::update();
     
-    double delta = global_timer_base::instance().recent_delta();
+    auto& timer = global_timer_base::instance();
     if (!_yielded || _yielded->is_done()) {
-        _yielded.reset(std::get<1>(_coroutine.resume<yieldable*>(delta)));
+        _yielded.reset(std::get<1>(_coroutine.resume<yieldable*>(timer.current_time(),
+                                                                 timer.recent_delta())));
     } else if (_yielded) {
-        _yielded->update(delta);
+        _yielded->update(timer.recent_delta());
     }
 }
