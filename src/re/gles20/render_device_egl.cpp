e3682583feeaa32842f8487fd763d0a9aa7d2091
Hao Wu <wuhao.wise@gmail.com>
2014-04-13 02:42:15 -0700

    clean/fix

--- src/re/gles20/render_device_egl.cpp
+++ src/re/gles20/render_device_egl.cpp
@@ -41,10 +41,6 @@ namespace gles20 {
             return false;
         }
 
-        if (!eglSwapInterval(_internal->display, 1)) {
-            return false;
-        }
-        
         std::cout << eglQueryString(_internal->display, EGL_EXTENSIONS) << std::endl;
         return true;
     }
@@ -79,11 +75,6 @@ namespace gles20 {
         // immediate make it the current context
         EGLSurface surface = egl_window->surface();
         eglMakeCurrent(_internal->display, surface, surface, context);
-        
-//        GLuint tex;
-//        glGenTextures(1, &tex);
-//        glBindTexture(GL_TEXTURE_2D, tex);
-//        glTexStorage2DEXT(GL_TEXTURE_2D, 1, GL_ALPHA8_EXT, 128, 128);
         
         GLint texture_units = 1;
         glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &texture_units);
