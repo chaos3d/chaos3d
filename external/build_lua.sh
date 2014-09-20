3bd8b514ada07cdb900416bd9bb21e6bde6cc42f
Hao Wu <wuhao.wise@gmail.com>
2014-09-20 00:29:29 -0700

    build lua dylib for macosx

--- external/build_lua.sh
+++ external/build_lua.sh
@@ -35,21 +35,29 @@ make INSTALL_TOP=`pwd`/install-x86_64 install
 
 mkdir -p output-ios output-macosx output-include
 lipo -create $(find install-*/lib -name "*.a") -o output-ios/liblua.$VERSION.a
-cp install-x86_64/include/*.h output-include/.
+cp install-arm64/include/* output-include/.
 
-make CC="xcrun -sdk macosx clang -arch x86_64 -mmacosx-version-min=10.7" CFLAGS="-Ofast -DLUA_USE_MACOSX" MYLIBS="-lreadline" MYLDFLAGS="-flto" clean generic
-make INSTALL_TOP=`pwd`/install-macosx install
-cp install-macosx/lib/liblua.a output-macosx/liblua.$VERSION.a
+make CC="xcrun -sdk macosx clang -arch x86_64 -mmacosx-version-min=10.7" CFLAGS="-Ofast -DLUA_USE_LINUX" MYLIBS="-lreadline" MYLDFLAGS="-flto" \
+    LUA_A="liblua.dylib" AR="xcrun -sdk macosx clang -arch x86_64 -dynamiclib -o" RANLIB="strip -u -r" clean generic
+make TO_LIB="liblua.dylib" INSTALL_TOP=`pwd`/install-macosx64 install
+cp install-macosx64/bin/luac output-macosx/luac64
+cp install-macosx64/bin/lua output-macosx/lua64
+
+make CC="xcrun -sdk macosx clang -arch i386 -mmacosx-version-min=10.7" CFLAGS="-Ofast -DLUA_USE_LINUX" MYLIBS="-lreadline" MYLDFLAGS="-flto" \
+    LUA_A="liblua.dylib" AR="xcrun -sdk macosx clang -arch i386 -dynamiclib -o" RANLIB="strip -u -r" clean generic
+make TO_LIB="liblua.dylib" INSTALL_TOP=`pwd`/install-macosx32 install
+cp install-macosx32/bin/luac output-macosx/luac32
+lipo -create $(find install-macosx*/lib -name "*.dylib") -o output-macosx/liblua.$VERSION.dylib
 
 mkdir -p ../liblua/$VERSION/
 if [ "$1" == "link" ]; then
     (cd output-ios && ln -s liblua.$VERSION.a liblua.a)
-    (cd output-macosx && ln -s liblua.$VERSION.a liblua.a)
+    (cd output-macosx && ln -s liblua.$VERSION.dylib liblua.dylib)
     (cd ../liblua/ && ln -sF $VERSION lua)
 fi
 
 cp output-ios/*.a ../libs/ios/.
-cp output-macosx/*.a ../libs/macosx/.
+cp output-macosx/* ../libs/macosx/.
 cp output-include/*.h output-include/*.hpp ../liblua/$VERSION/
 
 cd ..
