e5b43f1ea54b69a2cdd29984e4669844ea5b7fc6
Hao Wu <wuhao.wise@gmail.com>
2014-10-01 01:27:54 -0700

    update lua to support dlsym

--- external/build_lua.sh
+++ external/build_lua.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=5.1.5
-
-if [ -n "$2" ]; then
-    VERSION="$2"
-fi
+: ${DEV_DIR:=`xcode-select -p`}
+: ${VERSION:=5.1.5}
 
 rm lua.tar.gz
 curl -L http://www.lua.org/ftp/lua-$VERSION.tar.gz > lua.tar.gz
@@ -18,30 +15,70 @@ rm -rf lua-$VERSION
 tar xvf lua.tar.gz
 cd lua-$VERSION
 
-make CC="xcrun -sdk iphoneos clang -arch armv7" CFLAGS="-Ofast -DLUA_USE_POSIX" MYLDFLAGS="-flto" clean generic
+if [ $VERSION = "5.1.5" ]; then
+    patch -p0 <<EOF
+--- src/loadlib.c
++++ src/loadlib.c
+@@ -61,14 +61,11 @@
+ #include <dlfcn.h>
+ 
+ static void ll_unloadlib (void *lib) {
+-  dlclose(lib);
+ }
+ 
+ 
+ static void *ll_load (lua_State *L, const char *path) {
+-  void *lib = dlopen(path, RTLD_NOW);
+-  if (lib == NULL) lua_pushstring(L, dlerror());
+-  return lib;
++  return RTLD_MAIN_ONLY;
+ }
+EOF
+elif [ $VERSION = "5.2.3" ]; then
+    patch -p0 <<EOF
+--- src/loadlib.c
++++ src/loadlib.c
+@@ -61,14 +61,11 @@
+ #include <dlfcn.h>
+ 
+ static void ll_unloadlib (void *lib) {
+-  dlclose(lib);
+ }
+ 
+ 
+ static void *ll_load (lua_State *L, const char *path, int seeglb) {
+-  void *lib = dlopen(path, RTLD_NOW | (seeglb ? RTLD_GLOBAL : RTLD_LOCAL));
+-  if (lib == NULL) lua_pushstring(L, dlerror());
+-  return lib;
++  return RTLD_MAIN_ONLY;
+ }
+EOF
+fi
+
+make CC="env DEVELOPER_DIR=$DEV_DIR xcrun -sdk iphoneos clang -arch armv7" CFLAGS="-Ofast -DLUA_USE_POSIX -DLUA_DL_DLOPEN" MYLDFLAGS="-flto" clean generic
 make INSTALL_TOP=`pwd`/install-armv7 install
 
-make CC="xcrun -sdk iphoneos clang -arch armv7s" CFLAGS="-Ofast -DLUA_USE_POSIX" MYLDFLAGS="-flto" clean generic
+make CC="env DEVELOPER_DIR=$DEV_DIR xcrun -sdk iphoneos clang -arch armv7s" CFLAGS="-Ofast -DLUA_USE_POSIX -DLUA_DL_DLOPEN" MYLDFLAGS="-flto" clean generic
 make INSTALL_TOP=`pwd`/install-armv7s install
 
-make CC="xcrun -sdk iphoneos clang -arch arm64" CFLAGS="-Ofast -DLUA_USE_POSIX" MYLDFLAGS="-flto" clean generic
+make CC="env DEVELOPER_DIR=$DEV_DIR xcrun -sdk iphoneos clang -arch arm64" CFLAGS="-Ofast -DLUA_USE_POSIX -DLUA_DL_DLOPEN" MYLDFLAGS="-flto" clean generic
 make INSTALL_TOP=`pwd`/install-arm64 install
 
-make CC="xcrun -sdk iphonesimulator clang -arch i386 -mios-version-min=5.0" CFLAGS="-Ofast -DLUA_USE_POSIX" MYLDFLAGS="-flto" clean generic
+make CC="env DEVELOPER_DIR=$DEV_DIR xcrun -sdk iphonesimulator clang -arch i386 -mios-version-min=5.0" CFLAGS="-Ofast -DLUA_USE_POSIX -DLUA_DL_DLOPEN" MYLDFLAGS="-flto" clean generic
 make INSTALL_TOP=`pwd`/install-i386 install
 
-make CC="xcrun -sdk iphonesimulator clang -arch x86_64 -mios-version-min=5.0" CFLAGS="-Ofast -DLUA_USE_POSIX" MYLDFLAGS="-flto" clean generic
+make CC="env DEVELOPER_DIR=$DEV_DIR xcrun -sdk iphonesimulator clang -arch x86_64 -mios-version-min=5.0" CFLAGS="-Ofast -DLUA_USE_POSIX -DLUA_DL_DLOPEN" MYLDFLAGS="-flto" clean generic
 make INSTALL_TOP=`pwd`/install-x86_64 install
 
 mkdir -p output-ios output-macosx output-include
 lipo -create $(find install-*/lib -name "*.a") -o output-ios/liblua.$VERSION.a
 cp install-arm64/include/* output-include/.
 
-make CC="xcrun -sdk macosx clang -arch x86_64 -mmacosx-version-min=10.7" CFLAGS="-Ofast -DLUA_USE_LINUX" MYLIBS="-lreadline" MYLDFLAGS="-flto" \
+make CC="env DEVELOPER_DIR=$DEV_DIR xcrun -sdk macosx clang -arch x86_64 -mmacosx-version-min=10.7" CFLAGS="-Ofast -DLUA_USE_LINUX" MYLIBS="-lreadline" MYLDFLAGS="-flto" \
     LUA_A="liblua.dylib" AR="xcrun -sdk macosx clang -arch x86_64 -dynamiclib -o" RANLIB="strip -u -r" clean generic
 make TO_LIB="liblua.dylib" INSTALL_TOP=`pwd`/install-macosx64 install
 
-make CC="xcrun -sdk macosx clang -arch i386 -mmacosx-version-min=10.7" CFLAGS="-Ofast -DLUA_USE_LINUX" MYLIBS="-lreadline" MYLDFLAGS="-flto" \
+make CC="env DEVELOPER_DIR=$DEV_DIR xcrun -sdk macosx clang -arch i386 -mmacosx-version-min=10.7" CFLAGS="-Ofast -DLUA_USE_LINUX" MYLIBS="-lreadline" MYLDFLAGS="-flto" \
     LUA_A="liblua.dylib" AR="xcrun -sdk macosx clang -arch i386 -dynamiclib -o" RANLIB="strip -u -r" clean generic
 make TO_LIB="liblua.dylib" INSTALL_TOP=`pwd`/install-macosx32 install
 
