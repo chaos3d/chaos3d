088b59fb24c8bc195247967d52d3be8f91027494
Hao Wu <wuhao.wise@gmail.com>
2014-10-05 02:12:42 -0700

    add test script

---
+++ editor/script/bootstrap.lua
@@ -0,0 +1,12 @@
+require "script.calculator"
+require "chaos3d.wx"
+
+print "hello"
+
+-- Call wx.wxGetApp():MainLoop() last to start the wxWidgets event loop,
+-- otherwise the wxLua program will exit immediately.
+-- Does nothing if running from wxLua, wxLuaFreeze, or wxLuaEdit since the
+-- MainLoop is already running or will be started by the C++ program.
+wx.wxGetApp():MainLoop()
+
+os.exit()
