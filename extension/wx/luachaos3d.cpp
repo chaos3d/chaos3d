221ea9a1a0f8f1b91b545259fba10651b4c7e958
Hao Wu <wuhao.wise@gmail.com>
2014-10-05 23:44:17 -0700

    temp import

--- extension/wx/luachaos3d.cpp
+++ extension/wx/luachaos3d.cpp
@@ -48,5 +48,46 @@ extern "C" int luaopen_chaos3d_wx(lua_State *L) {
 }
 
 extern "C" int luaopen_chaos3d(lua_State *L) {
+#if 0
+    set_main_context(main_device()->create_context(main_window()));
+    main_context()->set_current();
+    
+    component_manager::initializer(
+                                   make_manager<com::transform_manager>(),
+                                   make_manager<scene2d::world2d_mgr>(0.02f, Eigen::Vector2f{0.f,0.f}),
+                                   make_manager<sprite2d::sprite_mgr>(main_device()),
+                                   make_manager<com::camera_mgr>(main_device(),
+                                                                 main_context())
+                                   );
+    scene2d::world2d_mgr::instance()
+    .set_position_iteration(3)
+    .set_velocity_iteration(8);
+    
+    asset_manager& asset_mgr = global_asset_mgr::instance();
+    // treat png as a texture
+    // need a render device
+    asset_mgr.add_from_bundle(png_asset_bundle::bundle(main_device(),
+                                                       locator::dir_locator::app_dir()).get());
+    
+    auto state = script::state::create();
+    
+    state->import()
+    .def("load_atlas", LUA_BIND((&texture_atlas::load_from<script::ref, asset_manager&>)))
+    .import<sprite2d::sprite_mgr&>("sprite_mgr", sprite2d::sprite_mgr::instance())
+    .import<scene2d::world2d_mgr&>("world2d", scene2d::world2d_mgr::instance())
+    .import<asset_manager&>("asset", global_asset_mgr::instance())
+    .import("render", main_device())
+    .import("root", &game_object::root())
+    .import<render_window*>("window", main_window())
+    .import<locator_mgr&>("locator", locator_mgr::instance())
+    ;
+    
+    script::def_render_device(state.get());
+    script::def_game_object(state.get());
+    script::def_eigen_math();
+    script::def_sprite2d(state.get());
+    script::def_asset();
+    script::def_event(state.get());
+#endif
     return 1;
 }
