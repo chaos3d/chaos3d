62a7f8cc24e8f874fafbc52dda2ecc2b1e9357f8
Hao Wu <wuhao.wise@gmail.com>
2014-10-18 16:42:05 -0700

    temp fix for template linking

---
+++ src/platform/lua_import.cpp
@@ -0,0 +1,71 @@
+#include "re/render_device.h"
+#include "re/native_window.h"
+#include "re/gles20/render_device.h"
+
+#include "script/type/def.h"
+#include "script/class_type.h"
+#include "script/lua_bind.h"
+#include "script/state.h"
+#include "script/lua_ref.h"
+#include "script/import_scope.h"
+#include "script/converter_ptr.h"
+
+#include "go/game_object.h"
+#include "com/sprite2d/sprite.h"
+#include "com/sprite2d/texture_atlas.h"
+#include "com/scene2d/world_box2d.h"
+
+#include "asset/asset_manager.h"
+#include "asset/asset_locator.h"
+
+#include <array>
+
+using namespace script;
+
+template class script::class_<native_window>;
+template class script::class_<render_device>;
+
+extern "C" void c3d_lua_import(lua_State *L) {
+#if 0
+    set_main_context(main_device()->create_context(main_window()));
+    main_context()->set_current();
+    
+    component_manager::initializer(
+                                   make_manager<com::transform_manager>(),
+                                   make_manager<scene2d::world2d_mgr>(0.02f, Eigen::Vector2f{0.f,0.f}),
+                                   make_manager<sprite2d::sprite_mgr>(main_device()),
+                                   make_manager<com::camera_mgr>(main_device(),
+                                                                 main_context())
+                                   );
+    scene2d::world2d_mgr::instance()
+    .set_position_iteration(3)
+    .set_velocity_iteration(8);
+    
+    asset_manager& asset_mgr = global_asset_mgr::instance();
+    // treat png as a texture
+    // need a render device
+    asset_mgr.add_from_bundle(png_asset_bundle::bundle(main_device(),
+                                                       locator::dir_locator::app_dir()).get());
+#endif
+    auto state = state::create(L);
+    
+    state->import("chaos3d")
+    .def("load_atlas", LUA_BIND((&texture_atlas::load_from<ref, asset_manager&>)))
+    .def_singleton_getter<sprite2d::sprite_mgr>("get_sprite_mgr")
+    .def_singleton_getter<scene2d::world2d_mgr>("get_world2d_mgr")
+    .def_singleton_getter<global_asset_mgr>("get_asset_mgr")
+    .def_singleton_getter<locator_mgr>("get_locator")
+    //.import("render", main_device())
+    //.import<render_window*>("window", main_window())
+    ;
+    
+    script::class_<native_window>::type()
+    ;
+    
+    script::def_render_device(state.get(), "chaos3d");
+    script::def_game_object(state.get(), "chaos3d");
+    script::def_eigen_math();
+    script::def_sprite2d(state.get(), "chaos3d");
+    script::def_asset();
+    script::def_event(state.get(), "chaos3d");
+}
--- src/platform/lua_module.cpp
+++ src/platform/lua_module.cpp
@@ -1,7 +1,6 @@
 #include "lua_module.h"
 
 #include "re/render_device.h"
-#include "re/gles20/render_device.h"
 
 #include "script/type/def.h"
 #include "script/class_type.h"
@@ -11,20 +10,17 @@
 #include "script/import_scope.h"
 #include "script/converter_ptr.h"
 
-#include "go/game_object.h"
-#include "com/sprite2d/sprite.h"
-#include "com/sprite2d/texture_atlas.h"
-#include "com/scene2d/world_box2d.h"
-
-#include "asset/asset_manager.h"
-#include "asset/asset_locator.h"
-
 #include "platform/launcher.h"
 
+#include <log4cxx/basicconfigurator.h>
 #include <array>
+
+IMPORT_CLASS_TYPE(render_device);
+IMPORT_CLASS_TYPE(native_window);
 
 using namespace script;
 
+extern "C" void c3d_lua_import(lua_State *L);
 extern "C" int luaopen_chaos3d(lua_State *L) {
     auto checker = &launcher::initialize;
     if (checker == nullptr) {
@@ -32,39 +28,15 @@ extern "C" int luaopen_chaos3d(lua_State *L) {
         return 0;
     }
     
-#if 0
-    set_main_context(main_device()->create_context(main_window()));
-    main_context()->set_current();
+    log4cxx::BasicConfigurator::configure();
+    log4cxx::Logger::getRootLogger()->setLevel(log4cxx::Level::getAll());
     
-    component_manager::initializer(
-                                   make_manager<com::transform_manager>(),
-                                   make_manager<scene2d::world2d_mgr>(0.02f, Eigen::Vector2f{0.f,0.f}),
-                                   make_manager<sprite2d::sprite_mgr>(main_device()),
-                                   make_manager<com::camera_mgr>(main_device(),
-                                                                 main_context())
-                                   );
-    scene2d::world2d_mgr::instance()
-    .set_position_iteration(3)
-    .set_velocity_iteration(8);
+    c3d_lua_import(L);
     
-    asset_manager& asset_mgr = global_asset_mgr::instance();
-    // treat png as a texture
-    // need a render device
-    asset_mgr.add_from_bundle(png_asset_bundle::bundle(main_device(),
-                                                       locator::dir_locator::app_dir()).get());
-#endif
     auto state = state::create(L);
-    
     state->import("chaos3d")
     .def("init", LUA_BIND(&launcher::initialize))
-    .def("load_atlas", LUA_BIND((&texture_atlas::load_from<ref, asset_manager&>)))
     .def_singleton_getter<launcher>("get_launcher")
-    .def_singleton_getter<sprite2d::sprite_mgr>("get_sprite_mgr")
-    .def_singleton_getter<scene2d::world2d_mgr>("get_world2d_mgr")
-    .def_singleton_getter<global_asset_mgr>("get_asset_mgr")
-    .def_singleton_getter<locator_mgr>("get_locator")
-    //.import("render", main_device())
-    //.import<render_window*>("window", main_window())
     ;
     
     class_<launcher>::type()
@@ -72,13 +44,6 @@ extern "C" int luaopen_chaos3d(lua_State *L) {
     .def("poll_event", LUA_BIND(&launcher::poll_event))
     .def("get_render_device", LUA_BIND(&launcher::get_render_device))
     ;
-    
-    script::def_render_device(state.get(), "chaos3d");
-    script::def_game_object(state.get(), "chaos3d");
-    script::def_eigen_math();
-    script::def_sprite2d(state.get(), "chaos3d");
-    script::def_asset();
-    script::def_event(state.get(), "chaos3d");
     
     lua_getglobal(L, "chaos3d");
     return 1;
