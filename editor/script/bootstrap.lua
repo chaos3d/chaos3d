2c4ce3b3ba98cf8a3eb5a29cad54fdc5fc6d9266
Hao Wu <wuhao.wise@gmail.com>
2014-10-18 20:20:38 -0700

    test lua

--- editor/script/bootstrap.lua
+++ editor/script/bootstrap.lua
@@ -1,8 +1,24 @@
+--require "wx"
+local c3d = require "chaos3d"
+local launcher = c3d.init();
+
+local win = launcher:create_game_window("chaos3d", {0,0,1024,768});
+local rd = launcher:get_render_device(2);
+local rwin = rd:new_window(win, {0,0,1024,768}, 2);
+local context = rd:new_context(rwin);
+context:set_current();
+
+--collectgarbage();
+while true do
+    launcher:poll_event();
+end
+--[[
 require "calculator"
 require "chaos3d.wx"
 
 print "hello"
-
+print(c3d, c3d.get_sprite_mgr(), c3d.go.root)
+print(c3d.blend, blend)
 -- Call wx.wxGetApp():MainLoop() last to start the wxWidgets event loop,
 -- otherwise the wxLua program will exit immediately.
 -- Does nothing if running from wxLua, wxLuaFreeze, or wxLuaEdit since the
@@ -10,3 +26,4 @@ print "hello"
 wx.wxGetApp():MainLoop()
 
 os.exit()
+--]]
