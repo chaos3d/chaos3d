0cd9c0dd11ff2ebd4b3f558a6c0751ad5c05f6e0
Hao Wu <wuhao.wise@gmail.com>
2014-11-08 23:44:25 -0800

    optimize atlas frame by only assigning when there is a change

--- src/action_support/action_sprite.h
+++ src/action_support/action_sprite.h
@@ -21,11 +21,21 @@ namespace act {
     typedef sprite2d::quad_sprite::animated_atlas_key atlas_key_t;
     typedef animation_keyframe<atlas_key_t> atlas_anim_kf_t;
     typedef atlas_anim_kf_t::key_frames_t atlas_keyframes_t;
+    
     struct quad_atlas_applier_t {
         sprite2d::quad_sprite* _sprite;
+        mutable atlas_anim_kf_t::key_frame_t::key_t const* current = nullptr;
+        
+        quad_atlas_applier_t(sprite2d::quad_sprite* sprite)
+        : _sprite(sprite) {}
+        
         void operator() (atlas_anim_kf_t const& keyframes,
-                         timer::time_t offset) const {
+                         timer::time_t offset) {
             auto& key = keyframes.interpolate<void>(offset);
+            if (&key == current)
+                return;
+            
+            current = &key;
             _sprite->set_from_atlas(*key.atlas, key.name, key.mat);
             _sprite->mark_dirty();
         }
