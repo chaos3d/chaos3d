a4a4cfe71ae155383bb115501855917f18b7eba8
Hao Wu <wuhao.wise@gmail.com>
2014-11-16 23:00:18 -0800

    add more types

--- src/common/base_types.h
+++ src/common/base_types.h
@@ -7,7 +7,14 @@
 typedef Eigen::Vector2f vector2f;
 typedef Eigen::Vector3f vector3f;
 typedef Eigen::Vector4f vector4f;
+typedef Eigen::Affine3f affine3f;
+typedef Eigen::Matrix3f matrix3f;
 typedef Eigen::Matrix4f matrix4f;
+typedef Eigen::Translation3f translation3f;
+typedef Eigen::Quaternionf quaternionf;
+typedef Eigen::AngleAxisf angle_axisf;
+
 typedef Eigen::AlignedBox2i rect2d;
+typedef Eigen::AlignedBox2f box2f;
 
 #endif
