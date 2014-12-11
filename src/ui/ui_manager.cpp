10d172ebe34af398e783ff77361096aedfd885fa
Hao Wu <wuhao.wise@gmail.com>
2014-12-11 01:34:59 -0800

    ui design comment

--- src/ui/ui_manager.cpp
+++ src/ui/ui_manager.cpp
@@ -2,7 +2,9 @@
 #include "com/scene2d/world_box2d.h"
 #include "event/touch_event.h"
 
+#if 0
 ui_manager::ui_manager()
 : mgr_base(false) {
     
 }
+#endif
--- src/ui/ui_manager.h
+++ src/ui/ui_manager.h
@@ -1,41 +1,39 @@
-#ifndef _UI_UI_MANAGER_H
-#define _UI_UI_MANAGER_H
+#ifndef _CHAOS3D_UI_UI_MANAGER_H
+#define _CHAOS3D_UI_UI_MANAGER_H
 
 #include <vector>
 #include <Eigen/Geometry>
-#include "com/render/camera.h"
 #include "event/event_dispatcher.h"
 #include "event/touch_event.h"
 #include "go/component_manager.h"
+#include "re/native_window.h"
+
+namespace com {
+    class camera;
+}
 
 class ui_control;
 
-class ui_manager : public component_manager_base<ui_manager> {
+/// ui manager to handle events and dispatch
+/// it will also dispatch wrapped events, i.e. ui_click
+/// ui_value_change, which will be generated from ui elements
+class ui_manager : public event_dispatcher {
+//: public component_manager_base<ui_manager> {
+    // TODO: may need to be component for ui elements
 public:
-    struct queryable {
-        virtual ~queryable() {};
-    };
-    
-    typedef std::unique_ptr<queryable> queryable_ptr;
-    typedef Eigen::Vector2f vector2f;
-    
-public:
-    ui_manager();
-    
-    template<class T>
-    void set_queryable(T *);
 
-    void set_camera(com::camera* cam) {
-        _camera = cam;
-    }
+public:
+    /// the main window to capture the input event
+    /// the camera to convert screen coord to world point
+    ui_manager(native_window*, com::camera*);
     
+protected:
     void touch_began(touch_began_event const&);
     void touch_moved(touch_moved_event const&);
     void touch_ended(touch_ended_event const&);
     
 private:
-    
-    queryable_ptr _queryable;
+    native_window::ptr _window;
     com::camera* _camera;
 };
 
