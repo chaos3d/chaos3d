199a27a5f285c94084fa3b286aecf87cc7beb7fb
Hao Wu <wuhao.wise@gmail.com>
2014-12-21 15:46:20 -0800

    add material asset

---
+++ src/asset_support/material_asset.cpp
@@ -0,0 +1,1 @@
+#include "asset_support/material_asset.h"
---
+++ src/asset_support/material_asset.h
@@ -0,0 +1,26 @@
+#ifndef _CHAOS3D_ASSET_SUPPORT_MATERIAL_ASSET_H
+#define _CHAOS3D_ASSET_SUPPORT_MATERIAL_ASSET_H
+
+#include "re/render_device.h"
+#include "re/material.h"
+#include "asset/asset_bundle.h"
+#include "asset/locator_asset_bundle.h"
+#include "asset/asset_manager.h"
+
+// texture asset handle
+template<>
+class asset_handle_base<material> : public functor_asset_handle<material> {
+public:
+    typedef functor_asset_handle<material> base_t;
+
+public:
+    asset_handle_base(loader_t const& loader)
+    : base_t(loader) {
+    }
+
+    // TODO: load from a meta config
+};
+
+typedef asset_handle_base<material> material_handle;
+
+#endif
